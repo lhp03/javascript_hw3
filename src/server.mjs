cd0cdb8b576f9dbf0e8b5478115718355f141480
lhp03 <60330965+lhp03@users.noreply.github.com>
2022-10-07 14:14:35 +0900

    file

--- src/server.mjs
+++ src/server.mjs
@@ -1,7 +1,34 @@
-import * as webLib from './web-lib.mjs';
+import * as webLib from "./web-lib.mjs";
 import * as path from "path";
 import * as fs from "fs";
 
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 // TODO: configure and start server
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const readConfig = (callback) => {
+  fs.readFile(path.join(__dirname, "config.json"), "utf-8", (err, data) => {
+    if (err) {
+      console.log("file read failed ", err);
+      return;
+    }
+    data = JSON.parse(data);
+    callback(data);
+  });
+};
+
+const setServer = (config) => {
+  const httpServer = new webLib.HTTPServer(
+    path.join(path.dirname(__dirname), config.root_directory),
+    config.redirect_map
+  );
+
+  console.log("SETSERVER");
+
+  httpServer.listen(3000, "localhost");
+};
+
+readConfig(setServer);
--- src/web-lib.mjs
+++ src/web-lib.mjs
@@ -2,14 +2,15 @@ import * as path from "path";
 import * as net from "net";
 import * as fs from "fs";
 import MarkdownIt from "markdown-it";
+import { send } from "process";
 
 const MIME_TYPES = {
-    "jpg" : "image/jpg",
-    "jpeg" : "image/jpeg",
-    "png" : "image/png",
-    "html" : "text/html",
-    "css" : "text/css",
-    "txt" : "text/plain"
+  jpg: "image/jpg",
+  jpeg: "image/jpeg",
+  png: "image/png",
+  html: "text/html",
+  css: "text/css",
+  txt: "text/plain",
 };
 
 /**
@@ -18,11 +19,11 @@ const MIME_TYPES = {
  * @return extension (String)
  */
 function getExtension(fileName) {
-    const formatPath = path.extname(fileName).toLowerCase();
-    if (formatPath.startsWith(".")) {
-        return formatPath.substring(1);
-    }
-    return formatPath;
+  const formatPath = path.extname(fileName).toLowerCase();
+  if (formatPath.startsWith(".")) {
+    return formatPath.substring(1);
+  }
+  return formatPath;
 }
 
 /**
@@ -32,98 +33,141 @@ function getExtension(fileName) {
  * @return: MIME type (String), undefined for unkwown MIME types
  */
 function getMIMEType(fileName) {
-    const ext = path.extname(fileName);
-    return ext.length > 0 ? MIME_TYPES[ext.substring(1)] : null;
+  const ext = path.extname(fileName);
+  return ext.length > 0 ? MIME_TYPES[ext.substring(1)] : null;
 }
 
 class Request {
-    constructor(reqStr) {
-        const [method, path] = reqStr.split(" ");
-        this.method = method;
-        this.path = path;
-    }
+  constructor(reqStr) {
+    const [method, path] = reqStr.split(" ");
+    this.method = method;
+    this.path = path;
+  }
 }
 
 class Response {
+  static STATUS_CODES = {
+    200: "OK",
+    308: "Permanent Redirect",
+    404: "Page Not Found",
+    500: "Internal Server Error",
+  };
 
-    static STATUS_CODES = {
-        200 : "OK",
-        308 : "Permanent Redirect",
-        404 : "Page Not Found",
-        500 : "Internal Server Error"
-    };
+  constructor(socket, statusCode = 200, version = "HTTP/1.1") {
+    this.sock = socket;
+    this.statusCode = statusCode;
+    this.version = version;
+    this.headers = {};
+    this.body = null;
+  }
 
-    constructor(socket, statusCode = 200, version = "HTTP/1.1") {
-        this.sock = socket;
-        this.statusCode = statusCode;
-        this.version = version;
-        this.headers = {};
-        this.body = null;
+  setHeader(name, value) {
+    this.headers[name] = value;
+  }
+
+  status(statusCode) {
+    this.statusCode = statusCode;
+    return this;
+  }
+
+  send(body) {
+    this.body = body ?? "";
+
+    if (!Object.hasOwn(this.headers, "Content-Type")) {
+      this.headers["Content-Type"] = "text/html";
     }
 
-    setHeader(name, value) {
-        this.headers[name] = value;
-    }
+    const statusCodeDesc = Response.STATUS_CODES[this.statusCode];
 
-    status(statusCode) {
-        this.statusCode = statusCode;
-        return this;
-    }
+    const headersString = Object.entries(this.headers).reduce(
+      (s, [name, value]) => {
+        return s + `${name}: ${value} \r\n`;
+      },
+      ""
+    );
 
-    send(body) {
-        this.body = body ?? "";
-      
-        if (!Object.hasOwn(this.headers, "Content-Type")) {
-            this.headers["Content-Type"] = "text/html";
-        }
+    this.sock.write(`${this.version} ${this.statusCode} ${statusCodeDesc}\r\n`);
+    this.sock.write(`${headersString}\r\n`);
+    this.sock.write(this.body);
 
-        const statusCodeDesc = Response.STATUS_CODES[this.statusCode];
-
-        const headersString = Object.entries(this.headers).reduce((s, [name, value]) => {
-            return s + `${name}: ${value} \r\n`;
-        }, "");
-
-        this.sock.write(`${this.version} ${this.statusCode} ${statusCodeDesc}\r\n`);
-        this.sock.write(`${headersString}\r\n`);
-        this.sock.write(this.body);
-
-        this.sock.end();
-    }
+    this.sock.end();
+  }
 }
 
 class HTTPServer {
-    constructor(rootDirFull, redirectMap) {
-        this.rootDirFull = rootDirFull;
-        this.redirectMap = redirectMap;
-        this.server = net.createServer(this.handleConnection.bind(this));
-    }
+  constructor(rootDirFull, redirectMap) {
+    this.rootDirFull = rootDirFull;
+    this.redirectMap = redirectMap;
+    this.server = net.createServer(this.handleConnection.bind(this));
+  }
 
-    listen(port, host) {
-        this.server.listen(port, host);
-    }
+  listen(port, host) {
+    this.server.listen(port, host);
+  }
 
-    handleConnection(sock) {
-        sock.on("data", data => this.handleRequest(sock, data));
-    }
+  handleConnection(sock) {
+    sock.on("data", (data) => this.handleRequest(sock, data));
+  }
 
-    handleRequest(sock, binaryData) {
-        const req = new Request(binaryData.toString());
-        const res = new Response(sock);
-        const reqPathFull = path.join(this.rootDirFull, req.path);
+  handleRequest(sock, binaryData) {
+    const req = new Request(binaryData.toString());
+    const res = new Response(sock);
+    const reqPathFull = path.join(this.rootDirFull, req.path);
 
-        // TODO: (see homework specification for details)
-        // 0. implementation can start here, but other classes / methods can be modified or added
-        // 1. handle redirects first
-        // 2. if not a redirect and file/dir does not exist send back not found
-        // 3. if file, serve file
-        // 4. if dir, generate page that lists files and dirs contained in dir
-        // 5. if markdown, compile and send back html
+    console.log(req);
+
+    // TODO: (see homework specification for details)
+    // 0. implementation can start here, but other classes / methods can be modified or added
+    // 1. handle redirects first
+    // 2. if not a redirect and file/dir does not exist send back not found
+    // 3. if file, serve file
+    // 4. if dir, generate page that lists files and dirs contained in dir
+    // 5. if markdown, compile and send back html
+
+    //redirect
+    if (Object.keys(this.redirectMap).includes(req.path)) {
+      console.log("redirct");
+      res.status(308);
+      res.setHeader("Location", this.redirectMap[req.path]);
+      res.send();
+    } else {
+      fs.access(reqPathFull, fs.constants.F_OK, (err) => {
+        if (err) {
+          //path (file or dir) does NOT exist in root directory!
+          //console.log("NOT Readable!");
+          res.status(404);
+          res.send("NOT FOUND!");
+        } else {
+          //path (file or dir) is readable
+          //console.log("Readable!");
+          fs.stat(reqPathFull, (err, stats) => {
+            const isDirectory = stats.isDirectory();
+            const isFile = stats.isFile();
+
+            if (isFile) {
+              res.status(200);
+              res.setHeader("Content-Type", getMIMEType(reqPathFull));
+              fs.readFile(reqPathFull, (err, data) => {
+                if (err) {
+                  res.status(500);
+                  res.send("INTERNAL ERROR!");
+                } else {
+                  res.send(data);
+                }
+              });
+            } else if (isDirectory) {
+              fs.readdir(reqPathFull, { withFileTypes: true }, (err, files) => {
+                if (err) {
+                  res.status(500);
+                  res.send("INTERNAL ERROR!");
+                }
+              });
+            }
+          });
+        }
+      });
     }
+  }
 }
 
-
-export {
-    Request,
-    Response,
-    HTTPServer
-};
+export { Request, Response, HTTPServer };
